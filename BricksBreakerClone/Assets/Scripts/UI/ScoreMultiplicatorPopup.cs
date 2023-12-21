5e4ce69b16032d3630b14901fa9b505450d356fb
Kirill Kostenkov <keeps@tamplier.home>
2023-12-21 15:45:23 +0100

    populate popup inner logic

--- BricksBreakerClone/Assets/Scripts/UI/ScoreMultiplicatorPopup.cs
+++ BricksBreakerClone/Assets/Scripts/UI/ScoreMultiplicatorPopup.cs
@@ -1,6 +1,40 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreMultiplicatorPopup : MonoBehaviour
 {
-  
+    [SerializeField]
+    private Button multiplier_1;
+    [SerializeField]
+    private Button multiplier_3;
+    [SerializeField]
+    private Button multiplier_5;
+
+    public event Action<int> MultiplicatorSelected;
+
+    private void Awake()
+    {
+        multiplier_1.onClick.AddListener(() => SelectMultiplicator(1));
+        multiplier_3.onClick.AddListener(() => SelectMultiplicator(3));
+        multiplier_5.onClick.AddListener(() => SelectMultiplicator(5));
+    }
+    
+    private void OnDestroy()
+    {
+        Unsubscribe();
+    }
+
+    private void SelectMultiplicator(int mult)
+    {
+        MultiplicatorSelected?.Invoke(mult);
+    }
+
+    private void Unsubscribe()
+    {
+        multiplier_1.onClick.RemoveAllListeners();
+        multiplier_3.onClick.RemoveAllListeners();
+        multiplier_5.onClick.RemoveAllListeners();
+    }
+    
 }
